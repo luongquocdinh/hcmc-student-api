1eb767fe87cce84e78bd897355a244c66b9baaa4
quocdinhcs <luongquocdinh.95@gmail.com>
2017-06-27 00:00:12 +0700

    api hot news

--- routes/hot_news.js
+++ routes/hot_news.js
@@ -1,78 +1,50 @@
-var express = require('express')
-var path = require('path')
-var formidable = require('formidable')
-var session = require('express-session')
-var fs = require('fs')
-var util = require('util');
+let express = require('express')
+let path = require('path')
+let formidable = require('formidable')
+let session = require('express-session')
+let fs = require('fs')
+let util = require('util');
+let moment = require('moment')
 
-var router = express.Router()
+let router = express.Router()
 
-var responseSuccess = require('./../helper/responseSuccess')
-var responseError = require('./../helper/responseError')
+let responseSuccess = require('./../helper/responseSuccess')
+let responseError = require('./../helper/responseError')
 
-var User = require('./../models/user')
-var Login = require('./../models/login')
-var News = require('./../models/news')
-var Activity = require('./../models/activity')
-var Gift = require('./../models/gift')
-var View = require('./../models/view')
-var Comment = require('./../models/comment')
+let User = require('./../models/user')
+let Login = require('./../models/login')
+let News = require('./../models/news')
 
-var sess;
-var per_page = 10;
+let per_page = 10;
 
-router.get('/', function (req, res) {
-    var page = req.query.page || 1
+router.get('/', (req, res) => {
+    let page = req.query.page || 1
     let response = []
-    var start = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
-     return new Promise(resolve => {
-           return View.aggregate(
-                [
-                    // {
-                    //     "$match": {
-                    //         updated_at: {
-                    //             "$gte": start,
-                    //         }
-                    //     }
-                    // },
-                    {
-                        "$group": {
-                            "_id": {
-                                news_id: "$news_id",
-                                type: "$type",
-                                topic_ascii: "$topic_ascii",
-                                title: "$title",
-                                brief: "$brief",
-                                thumbnail: "$thumbnail",
-                                content: "$content"
-                            },
-                            "value": {"$sum": "$count"}
-                        }
-                    },
-                    { "$sort": {"value" : -1} },
-                ],
-                function(err, result) {
-                    for (var i = 0; i < result.length; i++) {
-                        response.push({
-                            news: result[i]._id,
-                            views: result[i].value
-                        })
-                    }
-                    resolve(response);
-                }
-            )
-     }).then(data => {
-         let news = data.slice((page - 1)*per_page, per_page*page)
-         return res.json({
-             data: news,
-             error: null
-         })
-     }).catch(err => {
-         return res.json({
-             data: null,
-             error: err
-         })
-     })
+    News.find({})
+        .sort({views: -1})
+        .skip(per_page * (page - 1))
+        .limit(per_page)
+        .then(data => {
+            let response = []
+            data.map(r => {
+                response.push({
+                    id: r._id,
+                    topic_ascii: r.topic_ascii,
+                    title: r.title,
+                    brief: r.brief,
+                    thumbnail: r.thumbnail,
+                    datetime: r.datetime,
+                    views: r.views
+                })
+            })
+            let result = {
+                total_page: Math.ceil(data.length / per_page),
+                page,
+                per_page,
+                response
+            }
+            return res.json(responseSuccess("Tin Hot", result));
+        })
 })
 
-module.exports = router
+module.exports = router;
