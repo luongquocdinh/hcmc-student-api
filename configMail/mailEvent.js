6619f93d495c21e5a307e48b3656cc6e62c6cd63
dinhlq <dinhlq@9action.com>
2017-09-20 10:03:25 +0700

    fix configs mail register event

--- configMail/mailEvent.js
+++ configMail/mailEvent.js
@@ -8,7 +8,7 @@ module.exports = function (data, event) {
         text: 'Hi <b>' + data.name + '</b>',
         html: 
         `Cám ơn bạn <b> ` + data.name + `  </b>đã đăng kí tham gia sự kiện<b> ` + event.title + ` </b>.<br /> ` + 
-        `Thời gian bắt đầu từ <b>` +  moment(data.startDate).format('DD/MM/YYYY') + ` </b>đến<b> ` + moment(data.endDate).format('DD/MM/YYYY') + `</b><br />` +
+        `Thời gian bắt đầu từ <b>` +  moment(event.startDate).format('DD/MM/YYYY') + ` </b>đến<b> ` + moment(event.endDate).format('DD/MM/YYYY') + `</b><br />` +
         `Thân <br />` + 
         `HCMC ADMIN`
     };
